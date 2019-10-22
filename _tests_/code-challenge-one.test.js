55ac3a809ea663bc19b903329c1ec9ecf0018975
Abbey Masters <abbeykmasters@gmail.com>
2019-10-21 21:22:13 -0700

    seconds passing

--- _tests_/code-challenge-one.test.js
+++ _tests_/code-challenge-one.test.js
@@ -2,6 +2,13 @@ const add = require('../challenges/code-challenge-one');
 
 describe('testing adding date function', () => {
 
-  const theDate = new Date(2019, 5, 27, 4, 27, 19, 3);
+
+  it('can add seconds to a date', () => {
+    const theFinalDate = new Date(2019, 10, 21, 9, 20, 1, 4);
+    const theDiff = '10s';
+    const result = add(theFinalDate, theDiff);
+    expect(result).toEqual(new Date(2019, 10, 21, 9, 20, 11, 4));
+  });
+
 
 });
--- challenges/code-challenge-one.js
+++ challenges/code-challenge-one.js
@@ -1,14 +1,19 @@
 function add(date, diff) {
+  let theNewDate;
   let endChar = diff.slice(-1);
   let num = +(diff.slice(0, -1));
-  if(endChar === 'd') date.setDate(date.getDate() + num);
-  if(endChar === 'w') date.setDate(date.getDate() + (num * 7));
-  if(endChar === 'M') date.setMonth(date.getMonth() + num);
-  if(endChar === 'y') date.setFullYear(date.getFullYear() + num);
-  if(endChar === 's') date.getSeconds(date.getSeconds() + num);
-  if(endChar === 'm') date.setMinutes(date.getMinutes() + num);
-  if(endChar === 'h') date.setHours(date.getHours() + num);
-  return date;
+  
+  if(endChar === 's') theNewDate = new Date(date.setSeconds(date.getSeconds() + num));
+  if(endChar === 'm') theNewDate = new Date(date.setMinutes(date.getMinutes() + num));
+  if(endChar === 'h') theNewDate = new Date(date.setHours(date.getHours() + num));
+  if(endChar === 'd') theNewDate = new Date(date.setDate(date.getDate() + num));
+  if(endChar === 'w') theNewDate = new Date(date.setDate(date.getDate() + (num * 7)));
+  if(endChar === 'M') theNewDate = new Date(date.setMonth(date.getMonth() + num));
+  if(endChar === 'y') theNewDate = new Date(date.setFullYear(date.getFullYear() + num));
+  
+  console.log(theNewDate);
+  return theNewDate;
 }
+
 
 module.exports = add;
