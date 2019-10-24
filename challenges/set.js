443e94d65168b156cebdea35a228f0f9dd676e48
Abbey Masters <abbeykmasters@gmail.com>
2019-10-23 23:03:32 -0700

    finishing instance methods

--- challenges/set.js
+++ challenges/set.js
@@ -17,6 +17,39 @@ class Set {
     else return false;
   }
 
+  intersection(set) {
+    const newSet = new Set();
+    for(let i = 0; i < set.array.length; i++) {
+      if(set.array.includes(set.array[i])) newSet.add(set.array[i]);
+    }
+    return newSet;
+  }
+
+  union(set) {
+    const newSet = new Set();
+    for(let i = 0; i < set.array.length; i++) {
+      newSet.add(set.array[i]);
+    }
+    for(let j = 0; j < this.array.length; j++) {
+      newSet.add(this.array[j]);
+    }
+    return newSet;
+  }
+
+  difference(set) {
+    const newSet = new Set();
+    for(let i = 0; i < set.array.length; i++) {
+      newSet.add(set.array[i]);
+    }
+    for(let j = 0; j < this.array.length; j++) {
+      if(newSet.has(this.array[j])) newSet.remove(this.array[j]);
+      else if(!newSet.has(this.array[j])) newSet.add(this.array[j]);
+    }
+    return newSet;
+  }
+
+  
+
 }
 
 
